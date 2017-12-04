f426c284fdb9af69abad164181310de1d4f37abd
yanbinA <yanbin7019@outlook.com>
2017-12-04 09:23:58 +0800

    完整二叉树

--- tree/src/test/java/com/yanbin/tree/Test.java
+++ tree/src/test/java/com/yanbin/tree/Test.java
@@ -12,12 +12,33 @@ import java.util.Scanner;
 public class Test {
 
     public static void main(String[] args) {
-
-        while (true) {
-            System.out.print("Enter first letter of show('quit'),\ninsert, find, delete:");
+        boolean isRunner = true;
+        Tree<Integer> tree = new Tree<>();
+        while (isRunner) {
+            System.out.print("Enter first letter of show('quit'),\ninsert, find, delete:\n");
             String choice = getString();
             switch (choice) {
-                
+                case "i":
+                    System.out.print("Enter value to insert:");
+                    int value = getInt();
+                    tree.insert(new Node<>(value, value));
+                    tree.displayTree();
+                    break;
+                case "f":
+                    System.out.print("Enter key to find:");
+                    Node<Integer> node = tree.find(getInt());
+                    System.out.println("found:" + node);
+                    break;
+                case "d":
+                    System.out.print("Enter key to delete:");
+                    Node<Integer> delete = tree.delete(getInt());
+                    System.out.println("delete:" + delete);
+                    tree.displayTree();
+                    break;
+                case "quit":
+                    isRunner = false;
+                    System.out.println("Stop!!!");
+                    break;
             }
         }
 
